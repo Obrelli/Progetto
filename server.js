509cdc2c4599e8201ccb8bc3e04db085f0fc9cb4
Obrelli <daniele.obrelli@gmail.com>
2021-12-10 16:59:29 +0100

    fix

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mysql = require("mysql");
 const alert = require("alert");
 const http = require("http");
-const fs = require("fs");
-const arr =[];
+const bodyParser = require("body-parser");
+var arr=[];
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -15,6 +15,7 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
 
 
 http.createServer(function (req, res) {
@@ -31,13 +32,27 @@ http.createServer(function (req, res) {
 });
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 // index page
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
+app.use( express.static( "views" ) );
 
+function formatDate(date) {
+  var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
+}
 
 // departments page
 app.get('/login', async function (req, res) {
@@ -53,24 +68,49 @@ app.get('/login', async function (req, res) {
     })
 });
 
+
+
 app.get('/ric', async function(req,res){
   res.render('pages/ricerca.ejs');
 })
 
 app.get('/ricerca', async function(req,res){
-  con.query('CREATE VIEW id(id_locale, città, date, np) AS SELECT tipologia.id_locale, città, date_disponibili, numero_massimo_persone FROM tipologia,utente_locale WHERE città="'+req.query.città+'" && date_disponibili="'+req.query.data+'" && numero_massimo_persone="'+req.query.np+'"; SELECT id_locale FROM id',(err,rows) =>{
-    console.log(rows);
-    con.query('DROP IF EXISTS id;');
-    /*
-    if (rows[0] != undefined) {
-      console.log(rows);
-      //res.render('pages/home', {nome: rows[0].nome});
-  } else {
-      alert("Account non trovato!");
-      console.log(rows);
-      //res.render('pages/index');
-  } */  
+  con.query('CREATE OR REPLACE VIEW id(id_locale, nome_locale, costo, email, nome_tipologia, città, data, np) AS SELECT DISTINCT tipologia.id_locale, nome_locale, costo, email, nome_tipologia, città, date_disponibili, numero_massimo_persone FROM tipologia,utente_locale WHERE tipologia.id_locale=utente_locale.id_locale && città="'+req.query.città+'" && date_disponibili="'+req.query.data+'" && numero_massimo_persone="'+req.query.np+'"',(err,rows) =>{
+    con.query('SELECT DISTINCT * FROM id',(err,rows)=>{
+      con.query('SELECT DISTINCT COUNT(*) AS num FROM id', (err,r)=>{
+        if (rows[0] != undefined) {
+          rows.forEach(rows=>{
+            rows.data = formatDate(rows.data);             
+          })
+          res.render('pages/locali', {rows,r});
+          //res.send(r);  
+        } else {
+          alert("Account non trovato!");
+          console.log(rows);
+          //res.render('pages/index');
+      }
+      })  
+    })
+      con.query('DROP IF EXISTS id;', (err,r)=>{
+        if(err){
+
+        }else{
+
+        }
+      });
   })
+})
+
+app.get('/info', (req,rec) => {
+  con.query('DROP IF EXISTS id;', (err,r)=>{
+  con.query('CREATE OR REPLACE VIEW id(id_locale, nome_locale, descrizione) AS SELECT DISTINCT tipologia.id_locale, nome_locale, descrizione FROM utente_locale WHERE utente_locale.id_locale="'+req.query.id_locale+'" && id_tipologia="'+req.query.id_tipologia+'"', (err, rows)=>{
+    con.query('SELECT DISTINCT id_locale, nome_locale, descrizione FROM id', (err,r1)=>{
+      con.query('SELECT DISTINCT tipo_servizio FROM servizi WHERE id_tipologia="'+req.query.id_tipologia+'"', (err,r2)=>{
+        console.log(r1,r2);
+      })
+    })
+  })
+})
 })
 
 
@@ -80,12 +120,5 @@ console.log('Server is listening on port 8080');
 
 
 /*
-fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
- 
-    console.log("JSON file has been saved.");
-});
+
 */
